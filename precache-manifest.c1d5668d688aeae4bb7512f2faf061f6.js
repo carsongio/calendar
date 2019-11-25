f80af3df532a79d62e17a08728e0ca67f3c6b95b
Giovanni Cristofoletti <giovanni.cristofoletti@gmail.com>
2019-11-25 15:07:19 +0100

    Updates

---
+++ precache-manifest.c1d5668d688aeae4bb7512f2faf061f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2deff32d037d8ae6f66972d8cfda94d",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "c2e06b4d9683cff4d199",
+    "url": "/calendar/static/css/2.30861e7f.chunk.css"
+  },
+  {
+    "revision": "958b884e96d503f960f1",
+    "url": "/calendar/static/css/main.b8aa8cd4.chunk.css"
+  },
+  {
+    "revision": "c2e06b4d9683cff4d199",
+    "url": "/calendar/static/js/2.1839e058.chunk.js"
+  },
+  {
+    "revision": "958b884e96d503f960f1",
+    "url": "/calendar/static/js/main.a9ae76cf.chunk.js"
+  },
+  {
+    "revision": "eff2847e831f54dbc882",
+    "url": "/calendar/static/js/runtime-main.367dd98c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/calendar/static/media/logo.25bf045c.svg"
+  }
+]);
